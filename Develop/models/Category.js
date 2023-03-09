19945a7e7592eb9efabdbac03f08b9b004a40d00
Fabitheguy003 <fabienmukungu@gmail.com>
2023-03-08 22:22:08 -0500

    added remaining code category.js

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -19,6 +19,18 @@ Category.init(
     },
   },
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
     sequelize,
     timestamps: false,
     freezeTableName: true,
